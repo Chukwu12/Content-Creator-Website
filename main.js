202ac2615642b8d0eec718a04321667cc7f5b776
okey <39450490+Chukwu12@users.noreply.github.com>
2023-08-12 18:49:02 +0000

    update

--- main.js
+++ main.js
@@ -41,14 +41,14 @@ const interval = setInterval(() => {
 
 
 // ==========================================Home Background Effects=================================//
-const parallax = document.querySelector('.parallax');
+const parallax = document.querySelector('#home-page');
 const front = document.querySelector('.layer');
 const back = document.querySelector('.back-layer');
 
 const sFront = 150;
 const sBack  = 400;
 
-parallax.addEventListener('mousemove', e => {
+home-page.addEventListener('mousemove', e => {
   
   const x = e.clientX;
   const y = e.clientY;
