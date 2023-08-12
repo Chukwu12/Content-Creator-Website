436061a7214af52be8e8b57c176334f07c54bf4a
okey <39450490+Chukwu12@users.noreply.github.com>
2023-08-12 14:51:02 +0000

    update

--- main.js
+++ main.js
@@ -38,3 +38,62 @@ const interval = setInterval(() => {
     This is just a simple example*/
   }
 }, 1000);
+
+
+// ==========================================Home Background Effects=================================//
+export default class Grid {
+  constructor(el) {
+      this.DOM = {el: el};
+      this.gridItems = [];
+      this.items = [...this.DOM.el.querySelectorAll('.grid__item')];
+      this.items.forEach(item => this.gridItems.push(new GridItem(item)));
+     
+      this.showItems();
+  }
+  showItems() {
+    gsap.timeline()
+    .set(this.items, {scale: 0.7, opacity: 0}, 0)
+    .to(this.items, {
+        duration: 2,
+        ease: 'Expo.easeOut',
+        scale: 1,
+        stagger: {amount: 0.6, grid: 'auto', from: 'center'}
+    }, 0)
+    .to(this.items, {
+        duration: 3,
+        ease: 'Power1.easeOut',
+        opacity: 0.4,
+        stagger: {amount: 0.6, grid: 'auto', from: 'center'}
+    }, 0);
+  }  
+}
+
+const grid = new Grid(document.querySelector('.grid-pics'));
+
+
+class GridItem {
+  constructor(el) {
+      this.DOM = {el: el};
+      this.move();
+  }
+  move() {
+    // amount to move in each axis
+    let translationVals = {tx: 0, ty: 0};
+    // get random start and end movement boundaries
+    const xstart = getRandomNumber(15,60);
+    const ystart = getRandomNumber(15,60);
+   
+    // infinite loop
+    const render = () => {
+        // Calculate the amount to move.
+        // Using linear interpolation to smooth things out.
+        // Translation values will be in the range of [-start, start] for a cursor movement from 0 to the window's width/height
+        translationVals.tx = lerp(translationVals.tx, map(mousepos.x, 0, winsize.width, -xstart, xstart), 0.07);
+        translationVals.ty = lerp(translationVals.ty, map(mousepos.y, 0, winsize.height, -ystart, ystart), 0.07);
+       
+        gsap.set(this.DOM.el, {x: translationVals.tx, y: translationVals.ty});  
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
+}
+}
