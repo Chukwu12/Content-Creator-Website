e70d5a58aed35832e4a5ac57276973eade67696a
okey <39450490+Chukwu12@users.noreply.github.com>
2023-08-12 16:34:57 +0000

    update

--- main.js
+++ main.js
@@ -41,3 +41,15 @@ const interval = setInterval(() => {
 
 
 // ==========================================Home Background Effects=================================//
+document.addEventListener("mousemove", parallax);
+function parallax(e){
+  this.querySelectorAll('.layer').forEach(layer => {
+    const speed = layer.getAttribute('data-speed') 
+
+    const x = (window.innerWidth - e.pageX*speed)/100
+    const y = (window.innerHeight - e.pageY*speed)/100
+
+    layer.style.transform = `translateX{${x}px) translateY(${y}px)`
+
+  })
+}
