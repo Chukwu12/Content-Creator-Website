38fd48d3f983dfa615eb5fd061f59b66b8ef9038
Okechukwu Egeruoh <oegeruoh@gmail.com>
2025-03-30 06:35:14 -0400

    update

--- src/js/main.js
+++ src/js/main.js
@@ -243,3 +243,19 @@
 );
     });
 
+// =============================swiper js===================//
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper-container", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+});
